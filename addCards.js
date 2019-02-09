3b7c26e7ebe3720fcdfe72b4e50533b1649d5f36
Kelly Hancox <kellyhancox@Kelly-Hancox-Rules.local>
2019-02-09 02:50:56 -0500

    adding js for database

--- addCards.js
+++ addCards.js
@@ -1,23 +1,31 @@
-var ideaNames = document.getElementById('Ideaname');
-var ideaDescriptions = document.getElementById('IdeaDesc');
-
-function submitIdea(){
-	window.alert = null;
-alert('test'); // fail
-delete window.alert; // true
-alert('test'); // win
-}
-
+//var ideaNames = document.getElementById("Ideaname");
+//var ideaDescriptions = document.getElementById("IdeaDesc");
 
 function addIdea() {
-			//alert("Wow, thats alot of damage.");
+			alert("Wow, thats alot of damage.");
 			var name = document.getElementById("IdeaName").value;
 			var description = document.getElementById("IdeaDesc").value;
 
-			alert(name);
+			//alert(name);
 			//alert(description);
 			var table = document.getElementById("ideas");
 			var str = "<tr><th>" + name + "</th><th>" + description + "</th>";
 			table.innerHTML += str;
+
+			var firebaseRef = firebase.database().ref();
+
+			firebaseRef.child(name).set(description);
+
+			//CREATE unqique, randomly generated child name:
+			//firebaseRef.push().set(description);
+
+
 			return false;
+	}
+
+	function submitIdea(){
+		window.alert = null;
+	alert('test'); // fail
+	delete window.alert; // true
+	alert('test'); // win
 	}
